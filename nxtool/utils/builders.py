04a6f918f9a004f01e8b8d54b674eece3ed41475
Luchian Mihai <luchiann.mihai@gmail.com>
2024-11-25 10:31:07 +0200

    docs: update docstrings for builders.py

--- nxtool/utils/builders.py
+++ nxtool/utils/builders.py
@@ -9,33 +9,65 @@ from abc import ABC, abstractmethod
 from pathlib import Path
 from shutil import rmtree
 
+from nxtool.configuration import PathsStore
+
 class Builder(ABC):
-    def __init__(
-        self,
-        source: Path,
-        destination: Path
-    ) -> None:
+    """
+    Abstract base class for a builder responsible for configuring, building,
+    installing, and cleaning build processes for a specified source and
+    destination.
+
+    Attributes:
+        source (Path): Path to the source directory for the build.
+        destination (Path): Path to the destination directory for build outputs.
+    """
+
+    def __init__(self, source: Path, destination: Path) -> None:
+        """
+        Initialize the Builder with a source and destination path.
+
+        :param source: Path to the source directory for the build.
+        :type source: Path
+        :param destination: Path to the destination directory for build outputs.
+        :type destination: Path
+        """
         self.source = source
         self.destination = destination
 
-
     @abstractmethod
     def configure(self, config: str):
-        pass
+        """
+        Configure the build environment based on the provided configuration.
+
+        :param config: Configuration settings for the build environment.
+        :type config: str
+        """
 
     @abstractmethod
     def build(self, target: str = "all"):
-        pass
+        """
+        Execute the build process for the specified target.
+
+        :param target: The build target to compile. Defaults to "all".
+        :type target: str
+        """
 
     @abstractmethod
     def install(self):
-        pass
+        """
+        Install the built files to the designated destination.
+        """
 
     @abstractmethod
     def clean(self):
-        pass
+        """
+        Clean intermediate build files without removing the entire output.
+        """
 
     def fullclean(self):
+        """
+        Remove the entire output directory, including all built files.
+        """
         if self.destination.exists() and self.destination.is_dir():
             rmtree(self.destination)
 
@@ -56,22 +88,31 @@ class MakeBuilder(Builder):
         )
 
     def _run_make_cmd(self, args: list[str]) -> None:
-        cmd = ['make'] + args
+        cmd = [f"make -C {PathsStore.nxtool_root}/nuttx"] + args
+        self._run_bash_cmd(cmd)
+
+    def _run_bash_cmd(self, args: list[str]) -> None:
+        cmd = ['bash', '-c'] + args
+        print(cmd)
         with subprocess.Popen(
-            cmd, stdout=subprocess.PIPE, text=True
+            cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, bufsize=1
         ) as proc:
             if proc.stdout:
                 for line in iter(proc.stdout.readline, ''):
-                    sys.stdout.write(line)
+                    print(line)
                 proc.communicate()
 
     def configure(self, config: str):
         """
         equivalent to ./tools/configure.sh
         """
+        self._run_bash_cmd([
+            f"{PathsStore.nxtool_root}/nuttx/tools/configure.sh {config}"
+        ])
 
     def build(self, target: str = "all"):
         "run builder"
+        self._run_make_cmd([])
 
     def install(self):
         "install target"
