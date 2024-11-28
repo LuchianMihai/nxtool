db0f2284e036e32e7b48bee9a731f9e278f4ee60
Luchian Mihai <luchiann.mihai@gmail.com>
2024-11-28 10:36:10 +0200

    feat: update workspace command

--- nxtool/commands/workspace.py
+++ nxtool/commands/workspace.py
@@ -1,3 +1,5 @@
+import importlib.resources
+from pathlib import Path
 import shutil
 import os
 
@@ -6,70 +8,71 @@ from typing_extensions import Annotated
 import typer
 
 from nxtool.utils.git import GitWrapper
-
-from nxtool.workspace import ProjectStore
 from nxtool.configuration import ConfigStore, PathsStore
 
 app = typer.Typer()
 
 @app.callback(invoke_without_command=True)
 def cb(
-    clone: Annotated[
+    update: Annotated[
         bool,
         typer.Option(
-            "--clone",
-            "-c",
+            "--update",
+            "-u",
             help="clone default repositories")
     ] = False
 ):
     """
     sub-command to manage workspace
     """
-    init: WorkspaceCmd = WorkspaceCmd(clone)
-    init.run()
+    cmd: WorkspaceCmd = WorkspaceCmd()
+
+    if update is True:
+        cmd.update()
+        return
+
+    cmd.init()
 
 class WorkspaceCmd():
-    def __init__(
-        self,
-        clone: bool = False
-                ):
+    def __init__(self):
         super().__init__()
-        self.clone = clone
-        self.cfg: ConfigStore = ConfigStore()
-        self.prj: ProjectStore = ProjectStore()
-
-    def _check_git(self) -> bool:
-        ret: bool = False
-
-        if shutil.which("git") is not None:
-            return True
-
-        return ret
-
-    def run(self):
-        """
-        Run the workspace initialization.
-        If -c flag is set, default repositories are also cloned
-        """
         if self._check_git() is False:
             print("git executable not found in path. Aborting")
             return
+
+    def _check_git(self) -> bool:
+        if shutil.which("git") is not None:
+            return True
+
+        return False
+
+    def init(self):
+        """
+        Run the workspace initialization.
+        """
 
         try:
             os.mkdir(PathsStore.nxtool_dir_name)
             print(f"{PathsStore.nxtool_dir_name} directory created")
 
-            self.cfg.dump()
-            self.prj.dump()
-
         except FileExistsError:
             print("Workspace already initialized. Aborting")
 
-        if self.clone is True:
-            # git clone https://github.com/apache/nuttx nuttx
-            nuttx_repo:GitWrapper = GitWrapper(f'{self.cfg.nuttx}')
-            nuttx_repo.clone()
+        data = importlib.resources.files("nxtool.data")
 
-            # git clone https://github.com/apache/nuttx-apps apps
-            apps_repo:GitWrapper = GitWrapper(f'{self.cfg.apps}')
-            apps_repo.clone()
+        cfg: Path = Path(str(data), "config.toml")
+        shutil.copy(str(cfg), PathsStore.nxtool_config)
+
+        prj: Path = Path(str(data), "projects.toml")
+        shutil.copy(str(prj), PathsStore.nxtool_projects)
+
+    def update(self):
+        cfg: ConfigStore = ConfigStore()
+
+        # git clone https://github.com/apache/nuttx nuttx
+        nuttx_repo:GitWrapper = GitWrapper(f'{cfg.nuttx}')
+        nuttx_repo.clone()
+
+        # git clone https://github.com/apache/nuttx-apps apps
+        apps_repo:GitWrapper = GitWrapper(f'{cfg.apps}')
+        apps_repo.clone()
